b25f77a8190e1d0da2d28742b0bd73c64ab7ff33
PawelSzy <peal1982@gmail.com>
2015-12-30 23:54:07 +0100

    dodaj wyswietlanie bledow przy zapisie form pacjent

--- app/controllers/pacjents_controller.rb
+++ app/controllers/pacjents_controller.rb
@@ -8,11 +8,18 @@ class PacjentsController < ApplicationController
 	end
 
 	 def create
-	    @pacjent = Pacjent.new(params[:pacjent])    # Not the final implementation!
+	    @pacjent = Pacjent.new(pacjent_params)    # Not the final implementation!
 	    if @pacjent.save
 	      # Handle a successful save.
 	    else
 	      render 'new'
 	    end
   end
+
+
+    def pacjent_params
+      params.require(:pacjent).permit(:imie, :nazwisko, :pesel, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
