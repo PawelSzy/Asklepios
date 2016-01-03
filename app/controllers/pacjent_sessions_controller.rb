85ba32d2c398762d99fe926777f6f299cd447ce3
PawelSzy <peal1982@gmail.com>
2016-01-03 23:45:19 +0100

    Zmien pasek logowanie gdy zalogowany

--- app/controllers/pacjent_sessions_controller.rb
+++ app/controllers/pacjent_sessions_controller.rb
@@ -9,6 +9,8 @@ class PacjentSessionsController < ApplicationController
     pacjent = Pacjent.find_by(pesel: params[:session][:pesel])
     if pacjent && pacjent.authenticate(params[:session][:password])
       # Zaloguj pacjenta i przekieruj go do strony pacjent pokaż
+      zaloguj_pacjent pacjent
+      redirect_to pacjent      
     else
       # Wyświetl błąd.
       flash.now[:danger] = 'Niewłaściwy Pesel Hub hasło'       
--- app/helpers/pacjent_sessions_helper.rb
+++ app/helpers/pacjent_sessions_helper.rb
@@ -1,2 +1,18 @@
 module PacjentSessionsHelper
+
+	# Zalogowuje pacjenta
+	def zaloguj_pacjent(pacjent)
+    	session[:pacjent_id] = pacjent.id
+  	end
+
+  # zwraca aktualnego pacjenta jeśli jest zalogowany lub jeśli istnieje
+  def aktualny_pacjent
+    @aktualny_pacjent ||= Pacjent.find_by(id: session[:pacjent_id])
+  end  	
+
+  # Zwraca True jeśli pacjent jest zalogowany
+  def zalogowany_pacjent?
+    !aktualny_pacjent.nil?
+  end
+
 end
