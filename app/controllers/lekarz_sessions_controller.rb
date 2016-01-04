f0b02c56acb365673ac24df9c87eb8860e18b77d
PawelSzy <peal1982@gmail.com>
2016-01-04 22:46:10 +0100

    Dodaj logowanie jako Lekarz

--- app/controllers/lekarz_sessions_controller.rb
+++ app/controllers/lekarz_sessions_controller.rb
@@ -4,4 +4,22 @@ class LekarzSessionsController < ApplicationController
 
   def new
   end
+
+  def create
+    lekarz = Lekarz.find_by(pesel: params[:session][:pesel])
+    if lekarz && lekarz.authenticate(params[:session][:password])
+      # Zaloguj lekarza i przekieruj go do strony lekarz pokaż
+      zaloguj_lekarz lekarz
+      redirect_to lekarz      
+    else
+      # Wyświetl błąd.
+      flash.now[:danger] = 'Niewłaściwy Pesel Hub hasło'       
+      render 'new'
+    end
+  end
+
+  def destroy
+    wyloguj_lekarza
+    redirect_to root_url
+  end  
 end
--- app/helpers/lekarz_sessions_helper.rb
+++ app/helpers/lekarz_sessions_helper.rb
@@ -1,2 +1,24 @@
 module LekarzSessionsHelper
+	# Zalogowuje lekarza
+	def zaloguj_lekarz(lekarz)
+    	session[:lekarz_id] = lekarz.id
+  	end
+
+  # zwraca aktualnego lekarza jeśli jest zalogowany lub jeśli istnieje
+  def aktualny_lekarz
+    @aktualny_lekarz ||= Lekarz.find_by(id: session[:lekarz_id])
+  end  	
+
+  # Zwraca True jeśli lekarz jest zalogowany
+  def zalogowany_lekarz?
+    !aktualny_lekarz.nil?
+  end
+
+    def wyloguj_lekarza
+    session.delete(:lekarz_id)
+    @aktualny_lekarz  = nil
+  end
+
+
+
 end
