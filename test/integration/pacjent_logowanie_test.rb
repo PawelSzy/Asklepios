916cdb8c4e47be34585aedcab710a7df7195b7a6
PawelSzy <peal1982@gmail.com>
2016-01-04 20:05:29 +0100

    Dodaj testowanie logowania pacjenta

--- test/integration/pacjent_logowanie_test.rb
+++ test/integration/pacjent_logowanie_test.rb
@@ -12,20 +12,20 @@ class PacjentLogowanieTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  # def setup
-  #   @pacjent = pacjents(:Eustachy)
-  # end
+  def setup
+    @pacjent = pacjents(:Eustachy)
+  end
 
-  # test "Zaloguj się z właściwymi danymi" do
-  #   get zaloguj_path
-  #   post zaloguj_path, session: { pesel: 12345678902, password: 'haslotest' }
-  #   assert_redirected_to @pacjent
-  #   follow_redirect!
-  #   assert_template 'pacjent_sessions/new'
-  #   assert_select "a[href=?]", zaloguj_path, count: 0
-  #   assert_select "a[href=?]", wyloguj_path
-  #   # assert_select "a[href=?]", pacjent_path(@pacjent)
-  # end
+  test "Zaloguj się z właściwymi danymi" do
+    get zaloguj_path
+    post zaloguj_path, session: { pesel: 12345678902, password: 'haslotest' }
+    assert_redirected_to @pacjent
+    follow_redirect!
+    assert_template 'pacjents/show'
+    assert_select "a[href=?]", zaloguj_path, count: 0
+    assert_select "a[href=?]", wyloguj_path
+    # assert_select "a[href=?]", pacjent_path(@pacjent)
+  end
 
 
 end
