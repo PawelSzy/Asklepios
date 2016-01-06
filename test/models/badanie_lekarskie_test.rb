22090b09042251a7dd4718ebf1f9348909ee6051
PawelSzy <peal1982@gmail.com>
2016-01-06 22:07:41 +0100

    Proba naprawy bledy SQLite3 NOT NULL constraint failed

--- test/models/badanie_lekarskie_test.rb
+++ test/models/badanie_lekarskie_test.rb
@@ -4,4 +4,22 @@ class BadanieLekarskieTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+def setup
+    @badanie = BadanieLekarskie.new(
+      pokoj_id: 1,
+      lekarz_id: 1,
+      specjalizacja_id: 1,
+      data: DateTime.new(2005, 01, 01), 
+      godzina: 1,
+      pacjent_id: 1
+    	)
+  end
+
+ test "should be valid" do
+    assert @badanie.valid?
+  end
+
+
+
 end
