a314171b0332784bb5fe1755a648b21fce731a7e
PawelSzy <peal1982@gmail.com>
2016-01-09 17:34:09 +0100

    Pokaz wizyte/badanie lekarska

--- app/controllers/badanie_lekarskies_controller.rb
+++ app/controllers/badanie_lekarskies_controller.rb
@@ -1,12 +1,7 @@
 class BadanieLekarskiesController < ApplicationController
 
   def show
-    @badanie = BadanieLekarskie.find(1)
-    @specjalizacja_id = @badanie.specjalizacja_id
- #    pokoj_id
- #    lekarz_id
-	# pacjent_id	
+    @badanie = BadanieLekarskie.find(params[:id])
 
-    @specjalizacja = Specjalizacja.find(@specjalizacja_id)
   end
 end
