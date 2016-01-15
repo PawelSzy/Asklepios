dca1f33a47670105b15a88ecb8e4bc00ef180ed4
PawelSzy <peal1982@gmail.com>
2016-01-15 23:45:46 +0100

    Dodaj Ajax wyswietlanie lekarzy danej specjalizacji

--- app/controllers/zarejestruj_wizyte_controller.rb
+++ app/controllers/zarejestruj_wizyte_controller.rb
@@ -11,11 +11,13 @@ class ZarejestrujWizyteController < ApplicationController
 	end
 
 	def create
+		@specjalizacja = Specjalizacja.find(params[:specjalizacja_id])
+		@lekarze = Lekarz.find_by specjalizacja_id: @specjalizacja.id
    		respond_to do |format|
       		format.html
       		format.js
     	end		
 	end
 
-	
+
 end
