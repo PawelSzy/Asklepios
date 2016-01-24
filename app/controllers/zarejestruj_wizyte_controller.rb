34e0f69e97693d161d2d93da53c9df5efbb06e9d
PawelSzy <peal1982@gmail.com>
2016-01-24 12:00:38 +0100

    Poczatki Click na przycisku lekarza

--- app/controllers/zarejestruj_wizyte_controller.rb
+++ app/controllers/zarejestruj_wizyte_controller.rb
@@ -5,6 +5,7 @@ class ZarejestrujWizyteController < ApplicationController
 		# @lekarz = Lekarz.find(params[:id])
 		# specjalizacja_id = @lekarz.specjalizacja_id
 		# @specjalizacja = Specjalizacja.find(specjalizacja_id)
+		@temp  = [1,2,3]
 	end
 
 	def new
@@ -12,13 +13,19 @@ class ZarejestrujWizyteController < ApplicationController
 
 	def create
 		@specjalizacja = Specjalizacja.find(params[:specjalizacja_id])
-		@lekarze = Lekarz.where( "specjalizacja_id", @specjalizacja.id ).select("id")
-		@wizyty = BadanieLekarskie.where(lekarz_id: "2")
+		# @lekarze = Lekarz.where(specjalizacja_id: "1")
+	
+		# @lekarz= Specjalizacja.find(params[:lekarz_id]);
+		# puts @lekarz
+
+		@lekarze = Lekarz.where( specjalizacja_id:  @specjalizacja.id ).select("id")
+		# @wizyty = BadanieLekarskie.where(lekarz_id: "2")
 		# [1, 2, 3, 4].map(&:to_s)
 		@wizyty = BadanieLekarskie.where("lekarz_id IN (?)", @lekarze.map(&:id) )
-		@wizyty = @wizyty.map(&:specjalizacja_id)
 
-
+		@temp  = [5,6,6]
+		@wizyty_godzina = @wizyty.map(&:godzina)
+		@wizyty_data = @wizyty.map(&:data)
    		respond_to do |format|
       		format.html
       		format.js
