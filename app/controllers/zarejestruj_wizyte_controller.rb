a35abc6b5bf2497f165de4f6eafa953f80647738
PawelSzy <peal1982@gmail.com>
2016-01-26 22:28:08 +0100

    Dodawanie Ajaxaa

--- app/controllers/zarejestruj_wizyte_controller.rb
+++ app/controllers/zarejestruj_wizyte_controller.rb
@@ -39,6 +39,34 @@ class ZarejestrujWizyteController < ApplicationController
 
 		@wizyty_godzina = @wizyty.map(&:godzina)
 		@wizyty_data = @wizyty.map(&:data)
+
+		@buttonslekarzy = Array.new
+
+		#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+		# <%= button_to "Claim", {action: "claim", idea_id: idea.id, remote: true}, {class: 'btn btn-small'} %>
+		#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+		# @specjalizacja.each do |lekarz| 
+		# 	# nazwa_buttona = lekarz.imie+' '+lekarz.nazwisko
+		# 	# button = button_to nazwa_buttona , { id: "lekarz"+lekarz.id.to_s, remote: true}, { class: " btn btn-primary  btn-block przycisk_lekarza"} 
+		# 	button = "10"
+		# 	@buttonslekarzy.push(button)
+		# end 
+
+
+	# +"<div>"
+	# +"<ul>"
+	# +'<% @specjalizacja.lekarzs.each do |lekarz| %>'
+	# +"<il>"
+ #    +'<%= button_to lekarz.imie+' '+lekarz.nazwisko, { id: "lekarz"+lekarz.id.to_s, remote: true}, { class: " btn btn-primary  btn-block przycisk_lekarza"} %>'
+ #    +"</il>"
+	# +'<% end %>'
+	# +"</ul>"
+
+
+# '<%= submit_tag(lekarz.imie+' '+lekarz.nazwisko, class: " btn btn-primary  btn-block przycisk_lekarza") %>'
+
    		respond_to do |format|
       		format.html
       		format.js
